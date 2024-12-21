5727a5c639ae37e4be018d2f77c4471afbbe0a60
NHAN <nhantrung890@gmail.com>
2024-12-21 09:11:29 +0700

    commit before merge to

--- Customer_sweetsoft_tech_support/obj/Debug/net8.0/Customer_sweetsoft_tech_support.AssemblyInfo.cs
+++ Customer_sweetsoft_tech_support/obj/Debug/net8.0/Customer_sweetsoft_tech_support.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Customer_sweetsoft_tech_support")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76372911190e2267c5a1de1c14ac019abbc2cb23")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46a8d999e050553cc0bd684baf43a79b1b99bb43")]
 [assembly: System.Reflection.AssemblyProductAttribute("Customer_sweetsoft_tech_support")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Customer_sweetsoft_tech_support")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
