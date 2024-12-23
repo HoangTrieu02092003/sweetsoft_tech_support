0a42bcd2965bd98d99ca3c8e26eb6132aae0d711
PhamThiHoaiThuy <thuy.pth.63cntt@ntu.edu.vn>
2024-12-23 20:48:00 +0700

    Export excel

--- Customer_sweetsoft_tech_support/obj/Debug/net8.0/Customer_sweetsoft_tech_support.AssemblyInfo.cs
+++ Customer_sweetsoft_tech_support/obj/Debug/net8.0/Customer_sweetsoft_tech_support.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Customer_sweetsoft_tech_support")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c85fbea920e5daa004847e7bd7068de0a27daf0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cacd56db75d5b44ae00ca061e877dd571f411907")]
 [assembly: System.Reflection.AssemblyProductAttribute("Customer_sweetsoft_tech_support")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Customer_sweetsoft_tech_support")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
