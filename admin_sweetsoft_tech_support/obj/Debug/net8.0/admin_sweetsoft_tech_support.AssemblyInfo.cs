4d2beed9b427f6b825e9e07e3385adacf5a02508
NHAN <nhantrung890@gmail.com>
2024-12-23 09:44:35 +0700

    commit

--- admin_sweetsoft_tech_support/obj/Debug/net8.0/admin_sweetsoft_tech_support.AssemblyInfo.cs
+++ admin_sweetsoft_tech_support/obj/Debug/net8.0/admin_sweetsoft_tech_support.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("admin_sweetsoft_tech_support")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56a2bc3dddcbcbfec13a6b796c407b4dc91a48af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b634e07a9ec31f85645db453c97f972af71640d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("admin_sweetsoft_tech_support")]
 [assembly: System.Reflection.AssemblyTitleAttribute("admin_sweetsoft_tech_support")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
