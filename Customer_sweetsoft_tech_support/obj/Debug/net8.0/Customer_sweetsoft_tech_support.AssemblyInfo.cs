5d7aafa4c7d9d3d8cdc76994d72c6074cb3ddf87
NHAN <nhantrung890@gmail.com>
2024-12-24 08:39:11 +0700

    commit

--- Customer_sweetsoft_tech_support/obj/Debug/net8.0/Customer_sweetsoft_tech_support.AssemblyInfo.cs
+++ Customer_sweetsoft_tech_support/obj/Debug/net8.0/Customer_sweetsoft_tech_support.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Customer_sweetsoft_tech_support")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b634e07a9ec31f85645db453c97f972af71640d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4758df1cb36dd4cd930f4636510cbe4d60a1f185")]
 [assembly: System.Reflection.AssemblyProductAttribute("Customer_sweetsoft_tech_support")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Customer_sweetsoft_tech_support")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- admin_sweetsoft_tech_support/obj/Debug/net8.0/admin_sweetsoft_tech_support.AssemblyInfo.cs
+++ admin_sweetsoft_tech_support/obj/Debug/net8.0/admin_sweetsoft_tech_support.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("admin_sweetsoft_tech_support")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6f91b4bd88436dca4adcb2c7d66bad9d0019da3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4758df1cb36dd4cd930f4636510cbe4d60a1f185")]
 [assembly: System.Reflection.AssemblyProductAttribute("admin_sweetsoft_tech_support")]
 [assembly: System.Reflection.AssemblyTitleAttribute("admin_sweetsoft_tech_support")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
