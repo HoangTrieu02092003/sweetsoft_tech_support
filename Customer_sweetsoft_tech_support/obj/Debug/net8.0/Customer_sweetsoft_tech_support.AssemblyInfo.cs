28b9c2db3cb69a47bfd12286177614a670b4226c
hoainam1231 <ali.hoainam.0408@gmail.com>
2024-12-28 10:36:54 +0700

    caapj nhaatj

--- Customer_sweetsoft_tech_support/obj/Debug/net8.0/Customer_sweetsoft_tech_support.AssemblyInfo.cs
+++ Customer_sweetsoft_tech_support/obj/Debug/net8.0/Customer_sweetsoft_tech_support.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Customer_sweetsoft_tech_support")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4f979c347111b3fb59ad3372ec9c1249b4a4fe4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca0f96d1881befe909098b8afc77ac0b6d0ff8b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Customer_sweetsoft_tech_support")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Customer_sweetsoft_tech_support")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
