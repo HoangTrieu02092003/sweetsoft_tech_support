9d0e90f03489587a9125dd539cd447708c241a00
hoainam1231 <ali.hoainam.0408@gmail.com>
2025-01-05 08:50:31 +0700

    css cuss

--- admin_sweetsoft_tech_support/obj/Debug/net8.0/admin_sweetsoft_tech_support.AssemblyInfo.cs
+++ admin_sweetsoft_tech_support/obj/Debug/net8.0/admin_sweetsoft_tech_support.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("admin_sweetsoft_tech_support")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bc4d6b3f4baa8f62eef0cf0568bf39cbebe0146")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28340a5826fc1dc11e3696b69993891e37e4a086")]
 [assembly: System.Reflection.AssemblyProductAttribute("admin_sweetsoft_tech_support")]
 [assembly: System.Reflection.AssemblyTitleAttribute("admin_sweetsoft_tech_support")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
