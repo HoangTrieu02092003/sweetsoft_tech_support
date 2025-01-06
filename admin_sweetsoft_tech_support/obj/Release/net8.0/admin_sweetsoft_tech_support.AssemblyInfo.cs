628bf8bcb8c5edd7ead258a0a9b9e3f5fdf13630
NHAN <nhantrung890@gmail.com>
2025-01-06 14:00:42 +0700

    sửa lỗi faq chưa lấy ảnh

--- admin_sweetsoft_tech_support/obj/Release/net8.0/admin_sweetsoft_tech_support.AssemblyInfo.cs
+++ admin_sweetsoft_tech_support/obj/Release/net8.0/admin_sweetsoft_tech_support.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("admin_sweetsoft_tech_support")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3b3555a731c38cf1053300da1927bdc627bed83")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05c80eb6bbb742fe3d96faddff440228dbdf20d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("admin_sweetsoft_tech_support")]
 [assembly: System.Reflection.AssemblyTitleAttribute("admin_sweetsoft_tech_support")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
