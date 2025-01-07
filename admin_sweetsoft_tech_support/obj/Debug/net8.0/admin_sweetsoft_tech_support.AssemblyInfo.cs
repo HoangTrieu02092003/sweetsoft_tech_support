33bc3b245142a88b7f5b00f573508ce032f23569
NHAN <nhantrung890@gmail.com>
2025-01-07 12:09:36 +0700

    hoan thanh chatbox

--- admin_sweetsoft_tech_support/obj/Debug/net8.0/admin_sweetsoft_tech_support.AssemblyInfo.cs
+++ admin_sweetsoft_tech_support/obj/Debug/net8.0/admin_sweetsoft_tech_support.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("admin_sweetsoft_tech_support")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+628bf8bcb8c5edd7ead258a0a9b9e3f5fdf13630")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83ed652fded407eef3e96b1c864511886f809626")]
 [assembly: System.Reflection.AssemblyProductAttribute("admin_sweetsoft_tech_support")]
 [assembly: System.Reflection.AssemblyTitleAttribute("admin_sweetsoft_tech_support")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
