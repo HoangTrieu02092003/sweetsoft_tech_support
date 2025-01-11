cd7b5bf85702e24df2d358c9b50d47b0942e5d74
NgoGiaLam1 <lamngo14857@gmail.com>
2025-01-11 10:12:48 +0700

    Sửa thời gian, phân trang và giao diện

--- admin_sweetsoft_tech_support/Controllers/SessionsController.cs
+++ admin_sweetsoft_tech_support/Controllers/SessionsController.cs
@@ -23,6 +23,7 @@ namespace admin_sweetsoft_tech_support.Controllers
                 TempData["ReturnUrl"] = Request.Path.ToString();
                 return RedirectToAction("Login", "Admin");
             }
+
             var sessions = _context.TblSessions
                 .Include(s => s.User)
                 .AsQueryable();
@@ -43,7 +44,7 @@ namespace admin_sweetsoft_tech_support.Controllers
                 sessions = sessions.Where(s => s.ExpiresAt <= DateTime.Now);
             }
 
-            var pageSize = 6; // số lượng người dùng mỗi trang
+            var pageSize = 5; // số lượng session mỗi trang
             var skip = (page - 1) * pageSize;
 
             var requestContext = sessions
@@ -51,32 +52,18 @@ namespace admin_sweetsoft_tech_support.Controllers
                 .Skip(skip) // bỏ qua dữ liệu đã xem ở các trang trước
                 .Take(pageSize);
 
-            var totalUsers = await sessions.CountAsync();
-
+            var totalSessions = await sessions.CountAsync();
 
             // Tính tổng số trang
-            var totalPages = (int)Math.Ceiling(totalUsers / (double)pageSize);
+            var totalPages = (int)Math.Ceiling(totalSessions / (double)pageSize);
 
             // Chuyển dữ liệu sang View
             ViewData["name"] = name;
             ViewData["status"] = status;
             ViewData["TotalPages"] = totalPages;
             ViewData["CurrentPage"] = page;
-            return View(await requestContext.ToListAsync());
-        }
 
-        // Kết thúc Session
-        [HttpPost]
-        public IActionResult EndSession(int id)
-        {
-            var session = _context.TblSessions.Find(id);
-            if (session != null)
-            {
-                session.ExpiresAt = DateTime.Now;
-                _context.SaveChanges();
-                TempData["Message"] = "Phiên làm việc đã được kết thúc.";
-            }
-            return RedirectToAction(nameof(Index));
+            return View(await requestContext.ToListAsync());
         }
     }
 }
